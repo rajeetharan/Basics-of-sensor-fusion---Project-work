e21209b4839f0eddaaddcd8f4dabe9980a00cea5
rajeetharan <50480686+rajeetharan@users.noreply.github.com>
2019-08-23 10:01:54 +0100

    Update IMU_with _buttons.py

--- IMU_with _buttons.py
+++ IMU_with _buttons.py
@@ -1,76 +1,53 @@
 from altimu10v5.lsm6ds33 import LSM6DS33
 from altimu10v5.lis3mdl import LIS3MDL
 from altimu10v5.lps25h import LPS25H
+from time import sleep
 import calendar
 import numpy as np
 import time
-import os
-from gpiozero import Button
-import RPi.GPIO as GPIO
-from signal import pause
 
 
-
-    
-button_start = Button (26)
-button_end = Button (13)
 
 
 lsm6ds33 = LSM6DS33()
 lsm6ds33.enable()
+#enabling accelerometer and gyroscope.
 
 lis3mdl = LIS3MDL()
 lis3mdl.enable()
-
-lps25h = LPS25H
-lis3mdl.enable()
-
-
-if os.path.exists("IMU_Sensor_Readings.txt"):
-  os.remove("IMU_Sensor_Readings.txt")
-else:
-  pass
-
+#enabling magnetometer 
 
 
      
-def do_imu():
-    timestamped_sensor_readings = np.ndarray((0,), np.float32)
-    timestamp = calendar.timegm(time.gmtime())
-    timestamped_sensor_readings = np.append(timestamped_sensor_readings, lsm6ds33.get_accelerometer_g_forces())
-    timestamped_sensor_readings = np.append(timestamped_sensor_readings, lsm6ds33.get_accelerometer_angles())
-    timestamped_sensor_readings = np.append(timestamped_sensor_readings, lsm6ds33.get_gyroscope_raw())
-    write_fmt = " ".join("%4.2f" for _ in timestamped_sensor_readings)
-    timestamped_sensor_readings = np.append(float(timestamp),timestamped_sensor_readings)
+while True:
+    
+    timestamped_imu_readings = np.ndarray((0,), np.float32)
+    #creating and assigning an array to store readings
+    timestamp= time.time()*1000
+    #millisecond timestamping
+    
+    timestamped_imu_readings = np.append(timestamped_imu_readings, lsm6ds33.get_accelerometer_g_forces())
+    timestamped_imu_readings = np.append(timestamped_imu_readings, lsm6ds33.get_accelerometer_angles())
+    timestamped_imu_readings = np.append(timestamped_imu_readings, lsm6ds33.get_gyroscope_raw())
+    timestamped_imu_readings = np.append(timestamped_imu_readings, lis3mdl.get_magnetometer_raw())
+    #appending IMU readings to the array
+    
+    write_fmt = " ".join("%4.8f" for _ in timestamped_imu_readings)
+    #writing format of reading's decimal points
+    timestamped_imu_readings = np.append(float(timestamp),timestamped_imu_readings)
+    #adding readings row
     write_fmt += " %.0f"
-    with open("IMU_Sensor_Readings.txt", "ab") as f:
-        np.savetxt(f, np.expand_dims(timestamped_sensor_readings, axis=0),  fmt=write_fmt)
-
-    sleep(0.2)
-
-
-
-
-try:
-    while True:
-        if button_start.is_pressed :
-            print("Button_start pressed ")
-            do_imu()
-        else:
-            print("Start not pressed ")
-            
-        if button_end.is_pressed :
-            print("closing program")
-            exit()
+    #writing format 
+    with open("IMU_Readings.txt", "ab") as f:
+        np.savetxt(f, np.expand_dims(timestamped_imu_readings, axis=0),  fmt=write_fmt)
+        #saving file
         
-        else:
-            pass
         
 
-finally:                     
-    GPIO.cleanup() 
+    sleep(0.8)
+        
 
-pause()     
+    
 
     
     
