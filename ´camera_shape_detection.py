c3ac593822f66287d25094f3d097b7ad6545f4b2
rajeetharan <50480686+rajeetharan@users.noreply.github.com>
2019-08-23 10:15:12 +0100

    Update ´camera_shape_detection.py

--- ´camera_shape_detection.py
+++ ´camera_shape_detection.py
@@ -115,7 +115,7 @@ while True:
     #logging format of the results
       
     with open("camera reading {}.txt".format(datetime.now().strftime('%d-%m-%Y-%H-%M')), "ab") as ff:
-        np.savetxt(ff, np.expand_dims(timestamped_camera_readings, axis=0),fmt='%f')
+        np.savetxt(ff, np.expand_dims(timestamped_camera_readings, axis=0),fmt='%4.8f')
         #saving results into a file
         
     key = cv2.waitKey(1)
