7434dbefaf29f6b6e589cfdefbf2adf32463122b
rajeetharan <50480686+rajeetharan@users.noreply.github.com>
2019-08-30 15:56:34 +0300

    Update IMU.py

--- IMU.py
+++ IMU.py
@@ -1,21 +1,22 @@
 from altimu10v5.lsm6ds33 import LSM6DS33
 from altimu10v5.lis3mdl import LIS3MDL
 from altimu10v5.lps25h import LPS25H
-from time import sleep
 import calendar
 import numpy as np
 import time
+from time import sleep
+import datetime
 
-
-
+dt = str(datetime.datetime.now())
+#format of saving file name 
 
 lsm6ds33 = LSM6DS33()
 lsm6ds33.enable()
-#enabling accelerometer and gyroscope.
+#enabling above IC which responsible for accelerometer and gyroscope.
 
 lis3mdl = LIS3MDL()
 lis3mdl.enable()
-#enabling magnetometer 
+#enabling above IC which responsible for magnetometer 
 
 
      
@@ -32,20 +33,26 @@ while True:
     timestamped_imu_readings = np.append(timestamped_imu_readings, lis3mdl.get_magnetometer_raw())
     #appending IMU readings to the array
     
-  
-    timestamped_imu_readings = np.append(float(timestamp),timestamped_imu_readings)
-    #adding readings in row
     
-    #writing format 
-    with open("IMU_Readings.txt", "ab") as f:
-        np.savetxt(f, np.expand_dims(timestamped_imu_readings, axis=0),  fmt='%4.8f')
-        #saving file
-        #writing format and IMU reading's decimal points    
+    timestamped_imu_readings = np.append(float(timestamp),timestamped_imu_readings)
+    #adding sensor readings in a row
+    
+   # with open("IMU_Readings_muh.csv", "ab") as f:
+        #np.savetxt(f, np.expand_dims(timestamped_imu_readings, axis=0),  fmt="%4.8f" , delimiter=",")
+        
+    with open("IMU sensor reading {}.csv".format(dt), "ab") as ff:
+        np.savetxt(ff, np.expand_dims(timestamped_imu_readings, axis=0),fmt='%4.8f' , delimiter=",")
+        
+        #saving file in name of current time and in format of XXXX.XXXXXXXX seperated by , 
+        
+        
 
-    sleep(0.8)
+    sleep(0.2)
         
 
     
 
     
     
+
+
